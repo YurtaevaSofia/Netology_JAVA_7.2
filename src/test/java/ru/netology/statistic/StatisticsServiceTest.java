888d35a64f87da3efeae2c2aa450592c9033ccd2
coursar <Sonia@MacBook-Air-Sofa.local>
2020-07-04 20:35:17 +0300

    Initial commit

---
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -0,0 +1,23 @@
+package ru.netology.statistic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatisticsServiceTest {
+
+    StatisticsService service = new StatisticsService();
+
+    long[] incomesInBillions = {6, 12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12, 1, 2, 0};
+    @Test
+    void shouldfindMax() {
+
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+
+
+}
